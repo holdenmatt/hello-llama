9af6530193481d114bd8bea0db0991247ea59e8e
Matt Holden <holden.matt@gmail.com>
2024-07-23 12:50:22 -0600

    Add prettier, import sorting, vscode settings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
+import "./globals.css";
+
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
-import "./globals.css";
+
 import { cn } from "@/lib/utils";
 
 const fontSans = FontSans({
